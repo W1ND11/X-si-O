0bbad95e19d3fc8379677ba18ae8a41fd7982fe2
w1nd2003 <eugenboico54@gmail.com>
2023-04-24 10:00:39 +0200

    finish all bugs

--- game_logic.js
+++ game_logic.js
@@ -9,7 +9,8 @@ const X_TEXT = "X"
 let currentPlayer = Math.random() >= 0.5 ? O_TEXT : X_TEXT ;
 document.getElementById("xORo").innerHTML = currentPlayer
 let spaces = Array(9).fill(null)
-
+let counterPress = 0
+let lang = 'ru'
 
 const startGame = () => {
     boxes.forEach(box => box.addEventListener('click', boxClicked))
@@ -21,21 +22,41 @@ function boxClicked(e) {
     if(!spaces[id] && !playerHasWon()) {
         spaces[id] = currentPlayer
         e.target.innerText = currentPlayer
+        counterPress++
 
         if(playerHasWon()) {
-            playerText = `${currentPlayer} has won!`
-            //playerText = currentPlayer +' has won!'
+            var cc=''
+            if(lang=='ru'){
+                cc='Выиграл!'
+            } else {
+                cc='Has won!'
+            }
+            playerText = `${currentPlayer} ${cc}`
             document.getElementById("currentPlayer").innerHTML = playerText
             let winning_blocks = playerHasWon()
             winning_blocks.map( box => boxes[box].style.backgroundColor=winnerIndicator)
-            return
+        } 
+        else if(draw()) {
+            if(lang=='ru'){
+                playerText = "Ничья!"
+            } 
+            else {
+                playerText = "Draw!"
+            }
+            document.getElementById("currentPlayer").innerHTML = playerText
+        } 
+        else {
+            currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
+            cPlayer(currentPlayer)
         }
-
-        currentPlayer = currentPlayer == X_TEXT ? O_TEXT : X_TEXT
-        console.log(currentPlayer, typeof currentPlayer)
-        cPlayer(currentPlayer)
     }
+}
 
+function draw() {
+    if (!(playerHasWon()) && counterPress==9){
+        return true
+    }
+    return false;
 }
 
 const winningCombos = [
@@ -52,39 +73,44 @@ const winningCombos = [
 function playerHasWon() {
     for (const condition of winningCombos) {
         let[a,b,c] = condition
-        
-
         if(spaces[a] && (spaces[a] == spaces[b] && spaces[a] == spaces[c])) {
             return [a,b,c]
         }
     }
     return false
 }
-
+ 
 restartBtn.addEventListener('click', restart)
 
 function restart() {
     spaces.fill(null)
-    randPlayer(currentPlayer)
+    counterPress = 0
     boxes.forEach( box => {
         box.innerText = ''
         box.style.backgroundColor = ''
     })
-
     currentPlayer = randPlayer()
     cPlayer(currentPlayer)
+    if(lang=='ru'){
+        document.getElementById("currentPlayer").innerHTML = "Текущий игрок:"
+    }else{document.getElementById("currentPlayer").innerHTML = "Current player:"}
 }
 
 function changeText() {
+    lang = "en"
     document.getElementById("playerText").innerHTML = "X and O"
     document.getElementById("currentPlayer").innerHTML = "Current player:"
     document.getElementById("restartBtn").innerHTML = "Restart"
+    restart()
+    
 }
 
 function changeText2() {
+    lang = "ru"
     document.getElementById("playerText").innerHTML = "Крестики░Нолики"
     document.getElementById("currentPlayer").innerHTML = "Текущий игрок:"
     document.getElementById("restartBtn").innerHTML = "Перезагрузка"
+    restart()
 }
 
 function randPlayer() {
